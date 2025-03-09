a2c6a94aba2b3655f20f09072325c97a58fbc65c
Voronovy <markv@MarkLegion>
2025-03-09 19:38:41 -0400

    Started Backend Code

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,14 +1,40 @@
 import typing
-from routes import routes
-from flask import Flask
+import threading
+from threading import lock
+from flask import Flask, Blueprint, request, jsonify
+from file_upload.uploadHandlers import pdf_upload_handler, supported_file_types, upload_handler
+
+
 
 app = Flask(__name__)
 
-def start_app(a: Flask) -> Flask:
-    app.register_blueprint(routes)
-    return app
+Has_run = False
+
+@app.before_request
+def setup():
+    with lock:
+        if Has_run:
+            pass
+        else:
+            Has_run = True
+            pass
+            #Initialize DB and all that
+
+
+@app.route('/upload', methods=['POST'])
+def upload() -> str:  
+    #Call Authentication here
+    success = upload_handler(request.content_type, request.content_encoding)
+    #TODO
+
+    
+
+@app.route('/testing', methods=["GET"])
+def test() -> str: 
+    if request.content_type in supported_file_types:
+        return "OK"
+    return 'Hello World!'
 
 
 if __name__ == "__main__":
-    app = start_app(app)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- backend/api/file_upload/uploadHandlers.py
+++ backend/api/file_upload/uploadHandlers.py
@@ -1,6 +1,36 @@
 import typing
 
-supported_file_types = ["application/pdf"]
 
-def pdf_upload_handler(data: str, ):
-    return ""
+
+class pdf_upload_handler:
+    def __call__(self, data: str) -> bool:
+        """
+        Description:
+
+        Requires:
+        Query is a string for the
+        Modifies:
+        Effects:
+
+        @param data: String to be used for pdf
+        """
+        pass
+
+    
+#TODO: Determine true type of the param file
+def UploadHandler(content_type: str, file: str) -> bool:
+    """
+    Description:
+
+    Requires:
+    Query is a string for the
+    Modifies:
+    Effects:
+
+    @param content_type: String containing the content type from the request header
+    @param file: String containing the file data
+    """
+    supported_file_types = {"application/pdf" : pdf_upload_handler()}
+
+    if content_type in supported_file_types:
+        supported_file_types[content_type](file)
