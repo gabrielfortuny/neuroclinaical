02faae6ad5f7ffdd6569d36fe3b2aee41a8a80a3
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-03-10 10:01:21 -0400

    flask app running again

---
+++ backend/api/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
--- backend/api/databaseAccess.py
+++ backend/api/databaseAccess.py
@@ -1,46 +1,46 @@
-import json
-import typing
-import threading
+# import json
+# import typing
+# import threading
 
 
-class DatabaseCredentials:
-    """
-    Description:
-    Provides database access and stores credentials from env for fast access
+# class DatabaseCredentials:
+#     """
+#     Description:
+#     Provides database access and stores credentials from env for fast access
 
-    Invariants:
+#     Invariants:
 
-    """
-    _instance = None  # ✅ Shared instance
+#     """
+#     _instance = None  # ✅ Shared instance
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)  # ✅ Creates the instance only once
-        return cls._instance
+#     def __new__(cls):
+#         if cls._instance is None:
+#             cls._instance = super().__new__(cls)  # ✅ Creates the instance only once
+#         return cls._instance
 
-    def initialize(self, value):
-        """
-        Manually initializes the singleton instance.
-        Should be called only once after creation.
-        """
-        if hasattr(self, "_initialized"):  # Prevent re-initialization
-            raise RuntimeError("Singleton instance has already been initialized")
+#     def initialize(self, value):
+#         """
+#         Manually initializes the singleton instance.
+#         Should be called only once after creation.
+#         """
+#         if hasattr(self, "_initialized"):  # Prevent re-initialization
+#             raise RuntimeError("Singleton instance has already been initialized")
 
-        self.value = value
-        self._initialized = True
+#         self.value = value
+#         self._initialized = True
 
 
 
-def database_controller(query: str) -> json:
-    """
-    Description:
+# def database_controller(query: str) -> json:
+#     """
+#     Description:
 
-    Requires:
-    Query is a string for the
-    Modifies:
-    Effects:
+#     Requires:
+#     Query is a string for the
+#     Modifies:
+#     Effects:
 
-    @param query: String to be used for query, presumably a SQL query
-    """
+#     @param query: String to be used for query, presumably a SQL query
+#     """
 
-    return ""
+#     return ""
