814b0f5d884df748854f22462839e8df4010c204
Voronovy <markv@MarkLegion>
2025-03-23 17:01:10 -0400

    Changed Utils for New Implementation

--- backend/app/services/data_upload/uploadHandlers.py
+++ backend/app/services/data_upload/uploadHandlers.py
@@ -31,7 +31,7 @@ class pdf_upload_handler:
                 text.append(page.get_text())
         return "\n".join(text)
 
-    def __call__(self, data: str, p_id: int) -> bool:
+    def __call__(self, file_path: str, p_id: int, report: Report) -> bool:
         # TODO: Write Docstring
         """
         Description:
@@ -45,73 +45,10 @@ class pdf_upload_handler:
 
         @param data: String to be used for pdf
         """
-        try:
-            pdf_base64 = data
-            pdf_binary = base64.b64decode(pdf_base64)  # Decode Base64 Encoding
-            report = Report(
-                patient_id=p_id
-            )  # Create report object to extract ID in order to create a folder with same name
-            db.session.add(report)
-            db.session.commit()
-            os.makedirs(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}", exist_ok=True
-            )  # Create file path (It's ok if this already exists for a patient)
-            os.makedirs(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}",
-                exist_ok=False,
-            )  # Create folder with report ID (Should never exist)
-            # Create Report file in aformentioned folder
-            with open(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/report.pdf",
-                "wb",
-            ) as f:
-                f.write(pdf_binary)
-            report.filetype = "pdf"
-            report.filepath = f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}"  # Update report filepath attribute to fit new folder created
-
-            text = self.extract_text_from_pdf(
-                report.filepath
-            )  # Extract raw text from the docx
-
-            with open(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/report.txt",
-                "w",
-                encoding="utf-8",
-            ) as f:
-                f.write(text)
-            # Store raw text is same folder as report.txt for easy universal access
-            days_dict = extract_days_from_text(
-                text
-            )  # Extract all days from text into a dictionary
-            count = 0
-
-            # TODO: EXTRACT IMAGES
-
-            for day, content in days_dict.items():  # Store all days as files
-
-                day_number = int((day.split(" "))[1])
-
-                filename = f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/{day}.txt"
-                with open(filename, "w", encoding="utf-8") as f:
-                    f.write(content)
-            # HANDLE SEIZURE
-            seizures = handle_seizure_request(days_dict)
-            if not store_seizures_array(seizures, p_id):
-                raise sqlalchemy.exc.SQLAlchemyError
-
-            # HANDLE DRUGS
-            drugs = handle_drugadmin_request(days_dict)
-            if not store_drugs_array(drugs, p_id):
-                raise sqlalchemy.exc.SQLAlchemyError
-
-            # HANDLE SUMMARY
-            report.days = count
-            report.summary = handle_summary_request(text)
-            db.session.commit()  # Save changes to out DB "report" object
-            return True
-        except Exception as err:
-            return False
-
+        text = self.extract_text_from_pdf(
+            report.filepath
+        )  # Extract raw text from the docx
+        return text
 
 class docx_upload_handler:
     def extract_text_from_docx(self, file_path: str) -> str:
@@ -146,7 +83,7 @@ class docx_upload_handler:
 
         return "\n".join(extracted_text)
 
-    def __call__(self, data: str, p_id: int) -> bool:
+    def __call__(self, file_path: str) -> str:
         """
         Description:
 
@@ -160,83 +97,22 @@ class docx_upload_handler:
         @param p_id: The Patient ID this report belongs to
         @return: bool indicating successful execution
         """
-        try:
-            docx_base64 = data
-            docx_binary = base64.b64decode(docx_base64)  # Decode Base64 Encoding
-            report = Report(
-                patient_id=p_id
-            )  # Create report object to extract ID in order to create a folder with same name
-            db.session.add(report)
-            db.session.commit()
-            os.makedirs(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}", exist_ok=True
-            )  # Create file path (It's ok if this already exists for a patient)
-            os.makedirs(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}",
-                exist_ok=False,
-            )  # Create folder with report ID (Should never exist)
-            # Create Report file in aformentioned folder
-            with open(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/report.docx",
-                "wb",
-            ) as f:
-                f.write(docx_binary)
-            report.filetype = "docx"
-            report.filepath = f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}"  # Update report filepath attribute to fit new folder created
+        text = self.extract_text_from_docx(
+            file_path
+        )  # Extract raw text from the docx
+        return text
 
-            text = self.extract_text_from_docx(
-                report.filepath
-            )  # Extract raw text from the docx
-
-            with open(
-                f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/report.txt",
-                "w",
-                encoding="utf-8",
-            ) as f:
-                f.write(text)
-            # Store raw text is same folder as report.txt for easy universal access
-            days_dict = extract_days_from_text(
-                text
-            )  # Extract all days from text into a dictionary
-            count = 0
-
-            # TODO: EXTRACT IMAGES
-
-            for day, content in days_dict.items():  # Store all days as files
-
-                day_number = int((day.split(" "))[1])
-
-                filename = f"{current_app.config["UPLOAD_FOLDER"]}/{p_id}/{report.id}/{day}.txt"
-                with open(filename, "w", encoding="utf-8") as f:
-                    f.write(content)
-            # HANDLE SEIZURE
-            seizures = handle_seizure_request(days_dict)
-            if not store_seizures_array(seizures, p_id):
-                raise sqlalchemy.exc.SQLAlchemyError
-
-            # HANDLE DRUGS
-            drugs = handle_drugadmin_request(days_dict)
-            if not store_drugs_array(drugs, p_id):
-                raise sqlalchemy.exc.SQLAlchemyError
-
-            # HANDLE SUMMARY
-            report.days = count
-            report.summary = handle_summary_request(text)
-            db.session.commit()  # Save changes to out DB "report" object
-            return True
-        except Exception as err:
-            return False
+            
 
 
 supported_file_types = {
-    "application/pdf": pdf_upload_handler(),
-    "application/docx": docx_upload_handler(),
+    "pdf": pdf_upload_handler(),
+    "docx": docx_upload_handler(),
 }
 
 # DIRECTLY USABLE UPLOAD HANDLERS:
 
-
-def upload_controller(content_type: str, content: str, patient_id: int) -> bool:
+def upload_controller(content_ext: str, file_path: str, p_id: int, report: Report) -> bool:
     """
     Description: Preforms all steps needed to upload and analyze a document
 
@@ -251,8 +127,35 @@ def upload_controller(content_type: str, content: str, patient_id: int) -> bool:
     @param content: String containing the file data in base 64 encoding
     @param patient_id: The patient who this report will belong to
     """
+    text = ""
+    if content_ext in supported_file_types:
+        text = supported_file_types[content_ext](content, p_id)
+    else:
+        return False
+    with open(
+                f"{file_path}/report.txt",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write(text)
+    # Store raw text is same folder as report.txt for easy universal access
+    days_dict = extract_days_from_text(
+        text
+    )  # Extract all days from text into a dictionary
 
-    if content_type in supported_file_types:
-        supported_file_types[content_type](content, patient_id)
-        return True
-    return False
+    # TODO: EXTRACT IMAGES
+
+    # HANDLE SEIZURE
+    seizures = handle_seizure_request(days_dict)
+    if not store_seizures_array(seizures, p_id):
+        raise sqlalchemy.exc.SQLAlchemyError
+
+    # HANDLE DRUGS
+    drugs = handle_drugadmin_request(days_dict)
+    if not store_drugs_array(drugs, p_id):
+        raise sqlalchemy.exc.SQLAlchemyError
+
+    # HANDLE SUMMARY
+    report.summary = handle_summary_request(text)
+    db.session.commit()  # Save changes to out DB "report" object
+    return True
