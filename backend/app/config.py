067fcbb35827ca40edceeb810bd2058b2673e32d
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-04-01 19:12:02 -0400

    fixed dev container setup

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,18 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv("FLASK_DEBUG", "False") == "True"
-    
+
     # Set as absolute path in the container
-    UPLOAD_FOLDER = "/app/NeuroClin"
-    
+    UPLOAD_FOLDER = UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
+
     # Make sure logging is enabled
     FLASK_LOG_LEVEL = os.getenv("FLASK_LOG_LEVEL", "INFO")
-    
+
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_COOKIE_CSRF_PROTECT = False
 
