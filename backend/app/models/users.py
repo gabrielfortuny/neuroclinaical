89be4c112f5682f4738295da37589c4a58e5b17b
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-04-02 15:56:47 -0400

    user model handles password hashing and setting
    for better encapsulation

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,3 +1,9 @@
+from typing import List
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models.base import BaseModel, db
+from app.models.conversations import Conversation
+
+
 class User(BaseModel):
     __tablename__ = "users"
     username: str = db.Column(db.Text, nullable=False, unique=True)
@@ -8,3 +14,24 @@ class User(BaseModel):
     conversations: List["Conversation"] = db.relationship(
         "Conversation", back_populates="user", cascade="all, delete"
     )
+
+    def set_password(self, password: str) -> None:
+        """
+        Hash and a new password and updates it in the User's record.
+
+        Args:
+            password (str): The provided plaintext password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password_attempt: str) -> bool:
+        """
+        Check if the provided password hashes to the stored password hash.
+
+        Args:
+            password_attempt (str): The plaintext password to be checked.
+
+        Returns:
+            bool: True if the password is correct, false otherwise.
+        """
+        return check_password_hash(self.password_hash, password_attempt)
