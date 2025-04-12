76e98879101a71b49cef84d4f25a9e79988acc38
nehme-a <137248703+nehme-a@users.noreply.github.com>
2025-04-12 13:36:56 -0400

    fixed LTMFile Struct to work with backend

--- frontend/ios/Neuro ClinAIcal/Neuro ClinAIcal/LTMFile.swift
+++ frontend/ios/Neuro ClinAIcal/Neuro ClinAIcal/LTMFile.swift
@@ -8,21 +8,17 @@
 import SwiftUI
 
 struct LTMFile: Codable {
-    let createdAt: String
     let filePath: String
-    let fileType: String
-    let modifiedAt: String
+    let fileName: String
     let patientId: Int
     let reportId: Int
     let summary: String
     
     enum CodingKeys: String, CodingKey {
-        case createdAt = "created_at"
-        case filePath = "filepath"
-        case fileType = "filetype"
-        case modifiedAt = "modified_at"
+        case filePath = "file_path"
+        case fileName = "file_name"
         case patientId = "patient_id"
         case reportId = "report_id"
-        case summary
+        case summary = "summary"
     }
 }
