a6771e36ae1e4136fa7b99a58abe8d95249b34d8
Markian Voronovych <voronovy@umich.edu>
2025-04-18 19:08:05 -0400

    Chat complete, OPENAPI changes

--- backend/app/routes/chats.py
+++ backend/app/routes/chats.py
@@ -1,5 +1,34 @@
-from flask import Blueprint
+from backend.app.services.data_upload.nlpRequestHandler import handle_chat_request
+from flask import Blueprint, request, jsonify, send_file
 from flask_restful import Api, Resource
+from app.models import Patient, Report
+from app.services.data_upload.uploadHandlers import supported_file_types
+from app import db
+import os
 
-chats_bp = Blueprint("chats", __name__)
+chats_bp = Blueprint("chats", __name__, url_prefix="/chat")
 api = Api(chats_bp)
+
+@chats_bp.route("/{report_id}/messages", methods=["POST"])
+def send_message(report_id: int):
+    try:
+        report = db.session.get(report_id, Report)
+        if not report:
+            return 404
+        _, content_ext = os.path.splitext(report.file_name)
+        # Extract text from file
+        if content_ext not in supported_file_types:
+            return jsonify({"error": "Failed to retrieve reports"}), 500
+
+        extracted_text = supported_file_types[content_ext](report.file_path)
+        if not extracted_text:
+            return jsonify({"error": "Failed to retrieve reports"}), 500
+        
+    except Exception as e:
+        return jsonify({"error": "Failed to retrieve reports"})# User input
+    
+    response = handle_chat_request(extracted_text)
+    return jsonify({"response": "Failed to retrieve reports"}), 200
+    
+
+
